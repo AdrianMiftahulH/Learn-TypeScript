e9b40266db847405f88a101e140ff5bdfb612e77
AdrianMiftahulH <adrianmiftahul@gmail.com>
2023-02-19 17:11:37 +0700

    Learn Type Data

---
+++ Lesson01/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let myName;
+let meaningOgLife;
+let isLoading;
+let album;
+myName = "Adrian";
+meaningOgLife = 12;
+isLoading = true;
+album = true;
---
+++ Lesson01/src/main.ts
@@ -0,0 +1,20 @@
+// Learn Type Data
+
+let myName: string;
+let meaningOgLife: number;
+let isLoading: boolean;
+let album: any;
+
+// type data with or
+let postId: string | number;
+let isActive: number| boolean
+
+myName = "Adrian"
+meaningOgLife = 12
+isLoading =  true
+album = true
+
+const sum = (a: number, b:number) => {
+    return a + b
+}
+
