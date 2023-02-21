9280ecc5d24abadb56ff8ded6fb2a7b8b30b135e
AdrianMiftahulH <adrianmiftahul@gmail.com>
2023-02-21 11:06:03 +0700

    Learn Index Signatures and keyof Assertions

---
+++ Lesson06/build/js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+// Learn  Index Signatures and keyof Assertions
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+    Adrian: 42
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Pizza']);
+let prop = 'Pizza';
+console.log(todaysTransactions[prop]);
+const todayNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todayNet(todaysTransactions));
+// todaysTransactions.Pizza = 40
+console.log(todaysTransactions['Adrian']);
+const student = {
+    name: 'Adriana',
+    GPA: 3.8,
+    classes: [100, 200]
+};
+// console.log(student.test);
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+Object.keys(student).map(key => {
+    console.log(student[key]);
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'name');
